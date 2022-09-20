081f46caecf6c4d2182039fe10f05e163f928e86
Tom <softwaredevr@gmail.com>
2022-09-20 13:18:04 -0600

    testing

--- site.js
+++ site.js
@@ -2,9 +2,13 @@ $( document ).ready(function() {
     // Handler for .ready() called.
     console.log("tom ready");
 
-    //$.post( "test.php", { name: "John", time: "2pm" })
-  //.done(function( data ) {
-    //alert( "Data Loaded: " + data );
-  //});
+    $.post( "test.html", { name: "John", time: "2pm" })
+  .done(function( data ) {
+    alert( "Data Loaded: " + data );
+  });
+
+  $.getJSON( "test.html", function( json ) {
+    console.log( "JSON Data: " + json.users[ 3 ].name );
+   });
 
   });
