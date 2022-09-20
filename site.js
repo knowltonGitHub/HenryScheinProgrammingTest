c6fb246d8293517aafaf875ee6df279d287f2a00
Tom <softwaredevr@gmail.com>
2022-09-20 13:33:11 -0600

    testing

--- site.js
+++ site.js
@@ -4,10 +4,10 @@ $( document ).ready(function() {
 
     $.post( " https://cors.io/?index.html", { name: "John", time: "2pm" })
   .done(function( data ) {
-    alert( "Data Loaded: " + data );
+    console.log( "Data Loaded: " + data );
   });
 
-  $.getJSON( "index.html", function( json ) {
+  $.getJSON( "test.html", function( json ) {
     console.log( "JSON Data: " + json.users[ 3 ].name );
    });
 
