94081b743c7d756cc359f48f560ca3141248cb5c
Tom <softwaredevr@gmail.com>
2022-09-21 18:40:07 -0600

    Add files via upload

---
+++ site.js
@@ -0,0 +1,26 @@
+alert("an alert");
+
+console.log("in");
+
+//$( document ).ready(function() {
+    // Handler for .ready() called.
+    //console.log("tom ready");
+  //   $.ajax({
+  //     url: 'https://clinquant-khapse-98c558.netlify.app/test.json',
+  //     //type: 'post',
+  //     data: {
+  //         name: 'John Doe'
+  //     },
+  //     headers: {
+  //       "Access-Control-Allow-Origin" : '*',
+  //       "Cross-Origin-Resource-Policy": 'cross-origin'
+  //     },
+  //     dataType: 'json',
+  //     success: function (data) {
+  //         console.log(data);
+  //     }
+  // });
+
+  //});
+
+  console.log("out");
